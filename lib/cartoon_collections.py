e0d37ba4618f0a5b299a23bbd0381f9ecb4f10b2
KI JONG BYUN <kjbyun0@gmail.com>
2024-01-14 17:13:13 -0600

    Assignment Done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(l):
+    for index, name in enumerate(l):
+        print(f'{index+1}. {name}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(l):
+    return [f'{item.title()}!' for item in l]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(l):
+    for item in l:
+        if (len(item) > 4):
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(l):
+    for item in l:
+        if item in ['cheddar', 'gouda', 'camembert']:
+            return item
+    return None
+    
